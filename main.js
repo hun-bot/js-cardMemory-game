8f24745156c44a25e15b3d8a2c6abc0875bc50ee
Jeong Hun Nam <njh3421@gmail.com>
2022-10-14 15:25:11 +0900

    Fix filped Err
    If cards shape are same, they filped forever

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 const card=document.querySelectorAll(".card-memory")
 const counter=document.querySelectorAll(".counter")
+const showCounter=document.querySelector(".showCounter")
+
+// bug -> 같은 카드가 클릭되도 count가 실행됨, 여러개 선택시 함수가 정상동작을 하지 않음
 
 let hasFilpped=false
 let matched=[]
 let cardMatched=false
+let count=0
+let cardArr=[]
 
 card.forEach(e=>e.addEventListener("click",filpedCard))
 
@@ -15,10 +20,12 @@ function cardShuffle(){
 }
 cardShuffle()
 
+console.log(showCounter.innerText)
 
-
-function setupCounter() {
-  return
+function setupCounter(arr) {
+  count++
+  showCounter.innerText=count
+  arr=[]
 }
 
 
@@ -27,34 +34,61 @@ function filpedCard(){
   let selectedCardName=this.children[0].alt
   console.log(selectedCardName)
   matched.push(selectedCardName)
-  if(matched.length===2){
-    checkCard()
-    // disabled 줘서 다른 카드 못만지게 하기
-  }
   if(!hasFilpped){
     hasFilpped=true
     showingCard(this)
   }
+  checkCard(this)
 }
 
 function showingCard(props){
   let card=props
-  setTimeout(()=>{
-    card.classList.remove("filp")
-    card.classList.toggle("slowly")
-  },2000)
+  card.classList.add("filp")
   hasFilpped=false
+}
+function showingBackSideCard(props){
+  let card=props
+  console.log("카드 뒤집기",card)
+  setTimeout(()=>{
+    card.map(e=>{
+      e.classList.remove("filp")
+      e.classList.add("slowly")
+    })
+  },1000)
+  
+  hasFilpped=true
 }
 
 function checkCard(props){
-  // console.log(props)
+  cardArr.push(props)
+  if(cardArr.length===2){
+    cardArr.map(e=>console.log(e))
+  }
   if(matched.length===2){
     if(matched[0]===matched[1]){
       cardMatched=true
-      console.log('같다')
+      // Showing front side of the card
+      // showingFrontSide(cardArr)
+      setupCounter()
     }
+    else{
+      console.dir(cardArr)
+      showingBackSideCard(cardArr)
+    }
+    cardArr=[]
     cardMatched=false
     return matched=[]
   }
 }
-
+// function showingFrontSide(arr){
+//   console.log(arr)
+//   setTimeout(()=>{
+//     arr.map((e)=>{
+//       e.className="card-memory filp"
+//     })
+//     showingFrontSide()
+//   },1000)
+// }
+// if(cardMatched){
+//   showingFrontSide()
+// }
